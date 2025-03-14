f8611e50156494df5a93e5f685c5ceb560a6dd39
jgtech1205 <jgtech1205@gmail.com>
2025-03-14 08:26:12 -0400

    Initial commit with Next.js and Tailwind setup

---
+++ tulos_yt/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+   <div className="p-10">
+    <h2>Tulos ecommerce website</h2>
+   <Button>Demo Button</Button>
+   </div>
+
+  );
+}
