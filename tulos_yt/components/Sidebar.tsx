ba906dc4d02803a9f4c747f193cdc6bc422572bf
jgtech1205 <jgtech1205@gmail.com>
2025-03-18 15:31:10 -0400

    created backend

--- tulos_yt/components/Sidebar.tsx
+++ tulos_yt/components/Sidebar.tsx
@@ -6,6 +6,7 @@ import { headerData } from "@/constants";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 import SocialMedia from "./SocialMedia";
+import { useOutsideClick } from "@/hooks/useOutsideClick";
 
 interface SidebarProps {
   isOpen: boolean;
@@ -14,6 +15,7 @@ interface SidebarProps {
 
 const Sidebar: FC<SidebarProps> = ({ isOpen, onClose }) => {
   const pathname = usePathname();
+  const sidebarRef = useOutsideClick<HTMLDivElement>(onClose);
   return (
     <div
       className={`fixed inset-y-0 left-0 z-50 bg-darkColor/50 shadow-xl hoverEffect cursor-auto w-full ${
@@ -24,6 +26,7 @@ const Sidebar: FC<SidebarProps> = ({ isOpen, onClose }) => {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.4, delay: 0.3 }}
+        ref={sidebarRef}
         className="min-w-72 max-w-96 bg-darkColor text-white/70 h-full p-10 border-r border-r-white flex flex-col gap-6"
       >
         <div className="flex items-center justify-between">
