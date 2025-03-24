baace87c995ad0b5ee5c678093de0ce00d38494c
jgtech1205 <jgtech1205@gmail.com>
2025-03-24 17:10:11 -0400

    added loading state to product grid

--- tulos_yt/components/ProductGrid.tsx
+++ tulos_yt/components/ProductGrid.tsx
@@ -1,12 +1,54 @@
-import React from 'react'
-import HomeTabbar from './HomeTabbar'
+"use client";
+
+import React, { useEffect, useState } from "react";
+import HomeTabbar from "./HomeTabbar";
+import { productType } from "@/constants";
+import { client } from "@/sanity/lib/client";
+import { Product } from "@/sanity.types";
+import ProductCard from "./ProductCard";
 
 const ProductGrid = () => {
-  return (
-    <div className ="mt-10 flex flex-col items-center">
-        <HomeTabbar />
-    </div>
-  )
-}
+  const [selectedTab, setSelectedTab] = useState(productType[0]?.title || "");
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-export default ProductGrid
+  const query = `*[_type == 'product' && variant == $variant] | order(name asc)`;
+  const params = { variant: selectedTab.toLowerCase() }; 
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await client.fetch(query, params);
+        setProducts(await response);
+         
+      } catch (error) {
+        console.log("Product fetching Error", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData(); 
+  }, [selectedTab]);
+
+  return (
+    <div className="mt-10 flex flex-col items-center">
+      <HomeTabbar selectedTab={selectedTab} onTabSelect={setSelectedTab} />
+      {loading ? (
+        <div>
+          <span>Product is loading...</span>
+        </div>
+      ) : (
+        products?.map((product: Product) => (
+          <div key={product?._id}>
+            <ProductCard product={product} />
+          </div>
+        ))
+      )}
+    </div>
+  );
+  
+};
+
+export default ProductGrid;
