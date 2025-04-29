38e25cb2efd85a70ca9c49ef9c2c6b7cd4b46e2d
jgtech1205 <jgtech1205@gmail.com>
2025-04-29 06:16:36 -0400

    fixed line items error

--- tulos_yt/actions/createCheckoutSession.ts
+++ tulos_yt/actions/createCheckoutSession.ts
@@ -40,12 +40,15 @@ export async function createCheckoutSession(items: CartItem[], metadata: Metadat
           currency: "usd",
           unit_amount: Math.round(item.product.price! * 100),
           product_data: {
-            name: (item.product.name ?? "Unnamed Product") as string,
+            name: (item.product?.name ?? "Unnamed Product") as string,
             metadata: { id: item.product._id },
-            ...(item.product.images?.length ? { images: [urlFor(item.product.images[0]).url()] } : {}),
+            ...(item.product.images?.length
+              ? { images: [urlFor(item.product.images[0]).url()] }
+              : {}),
           },
         },
       })),
+      
     };
 
     if (customerId) {
