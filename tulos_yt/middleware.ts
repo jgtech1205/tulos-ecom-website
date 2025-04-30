12d9daf22d1cbe8ba1277568376e6c304e05d933
jgtech1205 <jgtech1205@gmail.com>
2025-04-30 13:49:40 -0400

    updated middleware

--- tulos_yt/middleware.ts
+++ tulos_yt/middleware.ts
@@ -1,12 +1,11 @@
 // middleware.ts
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
 
-export default clerkMiddleware((auth, req) => {
-  console.log(" Clerk middleware triggered on:", req.nextUrl.pathname);
-});
+export default authMiddleware();
+
 export const config = {
   matcher: [
-    "/((?!api|_next|.*\\..*).*)", 
-    "/studio/:path*"
+    "/((?!api|_next|.*\\..*).*)",
+    "/studio/:path*",
   ],
 };
