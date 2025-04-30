45ed3da697aee0540cbc98502ce87075f6276eb5
jgtech1205 <jgtech1205@gmail.com>
2025-04-30 14:08:30 -0400

    updated middlware import

--- tulos_yt/middleware.ts
+++ tulos_yt/middleware.ts
@@ -1,5 +1,5 @@
 // middleware.ts
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware } from "@clerk/nextjs/edge";
 
 export default clerkMiddleware((auth, req) => {
   console.log(" Clerk middleware triggered on:", req.nextUrl.pathname);
